4acb3297c64ecbeb0fcdb3a0ed95a5c23cfe7908
Steven Njoroge <stevenmacharia2003@gmail.com>
2026-04-06 14:24:21 +0300

    implemented the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { footerLinks } from "../constants";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='info'>
+        <p>More ways to shop: Find an Apple or other retailer near you or call Us</p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className='links'>
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({label, link}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+    </footer>
   )
 }
 
--- src/components/Perfomance.jsx
+++ src/components/Perfomance.jsx
@@ -68,7 +68,7 @@ const Performance = () => {
         <section id="performance" ref={sectionRef}>
             <h2>Next-level graphics performance. Game on.</h2>
 
-             <div className="wrapper">
+            <div className="wrapper">
                 {performanceImages.map((item, index) => (
                     <img
                         key={index}
@@ -77,7 +77,7 @@ const Performance = () => {
                         alt={item.alt || `Performance Image #${index + 1}`}
                     />
                 ))}
-             </div>
+            </div>
 
             <div className="content">
                 <p>
